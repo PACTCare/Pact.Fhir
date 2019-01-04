6ffa873308ac63a7b52b6184c11ccd705451b17f
Felandil <s.fuhrhop@hotmail.de>
2019-01-04 16:17:58 +0100

    add usecase skeleton

---
+++ Pact.Fhir.Core/Usecase/ResponseCode.cs
@@ -0,0 +1,9 @@
+﻿namespace Pact.Fhir.Core.Usecase
+{
+  public enum ResponseCode
+  {
+    Success = 1,
+
+    Failure = -1
+  }
+}
---
+++ Pact.Fhir.Core/Usecase/UsecaseInteractor.cs
@@ -0,0 +1,17 @@
+﻿namespace Pact.Fhir.Core.Usecase
+{
+  using Pact.Fhir.Core.Repository;
+
+  public abstract class UsecaseInteractor<TRequest, TResponse>
+    where TResponse : UsecaseResponse
+  {
+    public UsecaseInteractor(IFhirRepository repository)
+    {
+      this.Repository = repository;
+    }
+
+    protected IFhirRepository Repository { get; }
+
+    public abstract TResponse Execute(TRequest request);
+  }
+}
