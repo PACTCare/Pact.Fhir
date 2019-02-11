7c64a6edf432f76c71869b0857b982e000af3ef2
Felandil <s.fuhrhop@hotmail.de>
2019-02-11 16:46:55 +0100

    basic deterministic credential provider

---
+++ Iota/Pact.Fhir.Iota/Services/DeterministicCredentialProvider.cs
@@ -0,0 +1,48 @@
+﻿namespace Pact.Fhir.Iota.Services
+{
+  using Pact.Fhir.Iota.Entity;
+
+  using Tangle.Net.Cryptography;
+  using Tangle.Net.Cryptography.Signing;
+  using Tangle.Net.Entity;
+
+  public class DeterministicCredentialProvider : IChannelCredentialProvider
+  {
+    public DeterministicCredentialProvider(
+      Seed masterSeed,
+      IResourceTracker resourceTracker,
+      ISigningHelper signingHelper,
+      IAddressGenerator addressGenerator)
+    {
+      this.MasterSeed = masterSeed;
+      this.ResourceTracker = resourceTracker;
+      this.SigningHelper = signingHelper;
+      this.AddressGenerator = addressGenerator;
+    }
+
+    private IAddressGenerator AddressGenerator { get; }
+
+    private Seed MasterSeed { get; }
+
+    private IResourceTracker ResourceTracker { get; }
+
+    private ISigningHelper SigningHelper { get; }
+
+    /// <inheritdoc />
+    public ChannelCredentials Create()
+    {
+      var subSeed = new Seed(Converter.TritsToTrytes(this.SigningHelper.GetSubseed(this.MasterSeed, this.GetNextSubSeedIndex())));
+
+      return new ChannelCredentials
+               {
+                 Seed = new Seed(this.AddressGenerator.GetAddress(subSeed, SecurityLevel.Low, 0).Value),
+                 ChannelKey = this.AddressGenerator.GetAddress(subSeed, SecurityLevel.Low, 1).Value
+               };
+    }
+
+    private int GetNextSubSeedIndex()
+    {
+      return 0;
+    }
+  }
+}
