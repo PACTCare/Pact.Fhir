8e79a575abeca67092cb0cbdac55f77eb628ad82
Felandil <s.fuhrhop@hotmail.de>
2019-03-07 13:00:50 +0100

    basic batch processing

--- Pact.Fhir.Core/Usecase/BatchProcess/BatchProcessInteractor.cs
+++ Pact.Fhir.Core/Usecase/BatchProcess/BatchProcessInteractor.cs
@@ -1,10 +1,12 @@
 ﻿namespace Pact.Fhir.Core.Usecase.BatchProcess
 {
+  using System;
   using System.Collections.Generic;
   using System.Threading.Tasks;
 
   using Hl7.Fhir.Model;
 
+  using Pact.Fhir.Core.Entity;
   using Pact.Fhir.Core.Repository;
 
   public class BatchProcessInteractor : UsecaseInteractor<BatchProcessRequest, ResourceResponse>
@@ -16,17 +18,46 @@
     }
 
     /// <inheritdoc />
-    public override Task<ResourceResponse> ExecuteAsync(BatchProcessRequest request)
+    public override async Task<ResourceResponse> ExecuteAsync(BatchProcessRequest request)
     {
-      var responses = new List<Bundle.ResponseComponent>();
+      var responseBundle = new Bundle { Type = Bundle.BundleType.BatchResponse, };
+      responseBundle.PopulateMetadata("bundle-response", "bundle-response");
 
-      // loop through request bundle resources
-      // process bundle entry with given method
-      // add response to list
+      foreach (var entry in request.Bundle.Entry)
+      {
+        var resource = entry.Resource;
+        Bundle.ResponseComponent response = null;
 
-      // construct bundle with request resources and their matching responses
+        try
+        {
+          switch (entry.Request.Method)
+          {
+            case Bundle.HTTPVerb.GET:
+              resource = await this.Repository.ReadResourceAsync(entry.Resource.Id);
+              response = new Bundle.ResponseComponent { Status = "200 OK" };
+              break;
+            case Bundle.HTTPVerb.POST:
+              resource = await this.Repository.CreateResourceAsync(entry.Resource);
+              break;
+            case Bundle.HTTPVerb.PUT:
+              resource = await this.Repository.UpdateResourceAsync(entry.Resource);
+              break;
+            case Bundle.HTTPVerb.DELETE:
+              await this.Repository.DeleteResourceAsync(entry.Resource.Id);
+              break;
+            default:
+              break;
+          }
+        }
+        catch
+        {
 
-      return null;
+        }
+
+        responseBundle.Entry.Add(new Bundle.EntryComponent { Resource = resource, Response = response });
+      }
+
+      return new ResourceResponse { Code = ResponseCode.Success, Resource = responseBundle };
     }
   }
 }
